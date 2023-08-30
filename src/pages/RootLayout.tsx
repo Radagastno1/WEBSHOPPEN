c825b096d28746035e872478b03e59b0b2b527e4
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-08-30 21:19:03 +0200

    bakgrundbild istället för image element i header

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -5,8 +5,15 @@ import  ShoppingCartIcon  from '@mui/icons-material/ShoppingCart';
 export default function RootLayout(){
     return(
         <div className="flex flex-col h-screen">
-        <header className="flex h-40 min-h-700 bg-neutral-600 items-center justify-center">
-            <img src="https://i.imgur.com/nh4oD1B.jpg" alt="Picture with some plates and a vase" />
+          <header
+        className="flex h-40 min-h-700 bg-neutral-600 items-center justify-center"
+        style={{
+          backgroundImage: `url('https://i.imgur.com/nh4oD1B.jpg')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        {/* NAVLINKEN HÄR */}
         </header>
         <main className="bg-neutral-100 flex flex-1 flex-col"><div className="flex flex-col flex-1"><Outlet/></div></main>
         <footer className="h-10 bg-neutral-400"></footer>
