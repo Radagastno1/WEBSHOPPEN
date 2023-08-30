371d31dc787d049929cb2e18184011377d436ede
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-08-30 13:57:17 +0200

    confirmation page, css mostyly

--- src/pages/ConfirmationPage.tsx
+++ src/pages/ConfirmationPage.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import { Customer, useCustomerContext } from "../CustomerContext";
 import {
-  addCustomerToLS,
-  addProductToCart,
-  addProductToLS,
-  getCartFromLocalStorage,
-  getCustomerFromLS,
-  getProductsFromLS,
+    addProductToCart,
+    addProductToLS,
+    getCartFromLocalStorage,
+    getCustomerFromLS,
+    getProductsFromLS,
 } from "../localstorage";
 import { Products, mockedProducts } from "../mockedList";
 
@@ -47,31 +46,52 @@ export default function ConfirmationPage() {
     setProductsLoaded(true);
   }, []);
 
+  
   /////////////////////////////////////////////////////////////////////////
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex flex-col">
-        <h2>Din leveransadress</h2>
-
-        {customerLoaded ? (
-          <div>
-            <p>{customerRef.current?.name}</p>
-            <p>{customerRef.current?.address}</p>
-            <p>
-              {customerRef.current?.zipcode} {customerRef.current?.city}
-            </p>
-          </div>
-        ) : (
-          <p>Laddar uppgifter....</p>
-        )}
+    <div className="flex flex-col items-center" style={{ maxHeight: "calc(100vh - 200px)" }}>
+      <div className="mt-2 p-3 w-screen bg-neutral-700">
+        <h2>Order och leverans</h2>
       </div>
 
-      <div className="flex flex-col my-2">
-        <h2>Din order</h2>
+      <div className="flex bg-neutral-500 w-screen bg-opacity-5">
+        <div className="w-1/2 p-3">
+          <h2 className="font-bold">Order</h2>
+          <div>
+            <p>Ordernummer: 010101</p>
+            <p>Leveransmetod: Instabox</p>
+          </div>
+        </div>
 
+        <div className="w-1/2 p-3">
+          <h2 className="font-bold">Leverans</h2>
+          {customerLoaded ? (
+            <div>
+              <p>{customerRef.current?.name}</p>
+              <p>{customerRef.current?.address}</p>
+              <p>
+                {customerRef.current?.zipcode} {customerRef.current?.city}
+              </p>
+            </div>
+          ) : (
+            <p>Laddar uppgifter....</p>
+          )}
+        </div>
+      </div>
+
+      <div className="mt-2 p-3 w-screen bg-neutral-700">
+        <h2>Beställda produkter</h2>
+      </div>
+
+      <div className="flex flex-col bg-neutral-500 w-screen overflow-y-auto p-3 bg-opacity-5">
         {productsLoaded ? (
-          productsInCartRef.current.map((p) => <p key={p.id}>{p.title}</p>)
+          productsInCartRef.current.map((p) => (
+            <div key={p.id} className="flex items-center mb-2 my-2">
+              <img src={p.url} alt={p.title} className="w-10 h-10 mr-2" />
+              <p>{p.title}</p>
+            </div>
+          ))
         ) : (
           <p>Väntar på att produkter ska laddas...</p>
         )}
