a5f317ddd77a26ec378e33c4e225ec929a75e9a4
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-08-31 18:34:23 +0200

    nu klickar den rätt

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Products } from "../interfaces";
 import { addProductToLS } from "../localstorage";
 import { mockedProducts } from "../mockedList";
 import AddtoCartButton from "./AddtoCartButton";
-import { Products } from "../interfaces";
 
 export default function IndexPage() {
   const [selectedProduct, setSelectedProduct] = useState<Products | null>(null);
@@ -15,21 +15,19 @@ export default function IndexPage() {
   }, []);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center">
       <h1>SKORPA</h1>
-
       <ul>
         {mockedProducts.map((product) => (
-          <div data-cy="product">
-          <div key={product.id}>
-            <NavLink
+          <div key={product.id} data-cy="product" className="flex flex-1 flex-col my-0">
+            <NavLink className="flex flex-1 flex-col"
               to={`/product/${String(product.id)}`}
               onClick={() => setSelectedProduct(product)}
             >
-              {product.title}
+              <h1 className="text-2xl flex-1">{product.title}</h1>
+              <img src={product.image} alt="En katt" className="h-20 w-30"/>
             </NavLink>
-          </div>
-          <div data-cy="product-buy-button">
+          <div data-cy="product-buy-button" className="flex-1">
            <AddtoCartButton product={product} />
           </div>
           </div>
