e7f217c7c57f258daa0256eb13da28cc1f1df4a7
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-09-01 18:02:21 +0200

    customercontext använder uselocalstorage istälet för usestate

--- src/CustomerContext.tsx
+++ src/CustomerContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, ReactNode } from "react";
+import useLocalStorageState from "./useLocalStorage";
 
 export type Customer = {
   name: string;
@@ -14,17 +15,15 @@ type CustomerContextType = {
   setCustomer: (newCustomer: Customer) => void;
 };
 
-const CustomerContext = createContext<CustomerContextType | undefined>(undefined);
+const CustomerContext = createContext<CustomerContextType | undefined>(
+  undefined
+);
 
 export function CustomerProvider({ children }: { children: ReactNode }) {
-  const [customer, setCustomer] = useState<Customer>({
-    name: '',
-    address: '',
-    zipcode: '',
-    city: '',
-    email: '',
-    phone: '',
-  });
+  const [customer, setCustomer] = useLocalStorageState(
+    { name: "", address: "", zipcode: "", city: "", email: "", phone: "" },
+    "customer"
+  );
 
   //detta menas att alla barn i denna kommer åt customerprovidern och kan använda useCustomerContext
   return (
@@ -37,7 +36,9 @@ export function CustomerProvider({ children }: { children: ReactNode }) {
 export function useCustomerContext(): CustomerContextType {
   const context = useContext(CustomerContext);
   if (!context) {
-    throw new Error('to use customerContext you must place it inside CustomerProvider tagsen');
+    throw new Error(
+      "to use customerContext you must place it inside CustomerProvider tagsen"
+    );
   }
   return context;
 }
