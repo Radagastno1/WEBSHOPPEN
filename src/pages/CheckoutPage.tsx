7da09ebf716c035a50052a4120af3877a437d6a0
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-09-12 22:05:22 +0200

    removed media css and div-elements in checkoutpage, replaced with Box component from MUI and muis media queries

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,24 +1,41 @@
-import { Typography } from "@mui/material";
+import { Box, Typography, useMediaQuery } from "@mui/material";
 import FormComponent from "../components/CustomerCheckoutFormComponent";
 import { useCart } from "../contexts/CartContext";
 import CartPage from "./CartPage";
 
 export default function CheckoutPage() {
   const { cart } = useCart();
-
+  const isMobileScreen = useMediaQuery("(max-width: 500px)");
   return (
-    <div className="order-page-container flex flex-1 overflow-y-auto">
-      <div className="flex flex-col flex-1 items-center">
+    <Box
+      flexDirection={isMobileScreen ? "column" : "row"}
+      maxHeight={isMobileScreen ? "80vh" : "100%"}
+      sx={{
+        display: "flex",
+        flex: 1,
+        overflowY: "auto",
+      }}
+    >
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         {cart.length <= 0 ? (
           <Typography variant="h6">Din varukorg är tom</Typography>
         ) : (
           ""
         )}
         <CartPage />
-      </div>
-      <div className="flex flex-1 border-l-2 border-solid border-gray-300">
+      </Box>
+      <Box
+        sx={{ flex: 1, borderLeft: "2px solid gray", borderColor: "#e0e0e0" }}
+      >
         <FormComponent />
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 }
